42126284ab544523676e290c3fbf6b26c8c43552
Daniel Balcomb <daniel.balcomb@gmail.com>
2020-03-06 13:31:41 +0000

    Remove cert-manager module

--- modules/cert-manager/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "name" {
-  description = "The resource name"
-  value       = helm_release.main.metadata.0.name
-}
-
-output "metrics" {
-  description = "Enable prometheus metrics"
-  value       = var.metrics
-}
